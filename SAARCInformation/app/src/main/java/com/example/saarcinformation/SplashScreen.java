ed65326bd835f1859d7c2926b8a636e2150d4740
Ashraf Uddin <ashrafunahid@gmail.com>
2021-12-19 12:27:26 +0600

    Splash Screen Added

---
+++ SAARCInformation/app/src/main/java/com/example/saarcinformation/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.saarcinformation;
+
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+
+                    sleep(5000);
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        thread.start();
+
+    }
+}
