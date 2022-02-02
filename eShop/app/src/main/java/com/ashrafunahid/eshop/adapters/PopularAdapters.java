46f368d6c8842504b86c01ef4964f5ea089925e3
Ashraf Uddin <ashrafunahid@gmail.com>
2022-02-02 18:40:19 +0600

    .

--- eShop/app/src/main/java/com/ashrafunahid/eshop/adapters/PopularAdapters.java
+++ eShop/app/src/main/java/com/ashrafunahid/eshop/adapters/PopularAdapters.java
@@ -43,14 +43,14 @@ public class PopularAdapters extends RecyclerView.Adapter<PopularAdapters.ViewHo
         holder.rating.setText(popularModelsList.get(position).getRating());
         holder.discount.setText(popularModelsList.get(position).getDiscount());
 
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(context, ViewAllActivity.class);
-//                intent.putExtra("type", popularModelsList.get(position).getType());
-//                context.startActivity(intent);
-//            }
-//        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ViewAllActivity.class);
+                intent.putExtra("type", "New text");
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
